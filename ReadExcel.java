a1656a8aa0b2525081045f0dd037c6f471cc37c6
Sujathadinakaran <105733495+Sujathadinakaran@users.noreply.github.com>
2022-09-26 20:18:36 +0530

    Add files via upload

---
+++ ReadExcel.java
@@ -0,0 +1,14 @@
+package week6.day2;
+
+public class ReadExcel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Step1: setup the path for the workbook
+		XSSFWorkbook wb = new XSSFWorkbook("./day2/CreateLead.xlsx");
+		//step2: Get into the respective sheet
+		XSSFSheet ws = 
+	}
+
+}
